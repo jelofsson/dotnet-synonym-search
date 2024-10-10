d4c83a2df2c9c8a058b617e3b7b5f37211d46c3a
Jimmi Elofsson <contact@xrb.se>
2024-10-10 23:23:19 +0200

    Add CORS policy

--- backend/SynonymSearch/Program.cs
+++ backend/SynonymSearch/Program.cs
@@ -1,3 +1,5 @@
+var MyAllowSpecificOrigins = "_myAllowSpecificOrigins";
+
 var builder = WebApplication.CreateBuilder(args);
 
 // Add services to the container.
@@ -6,6 +8,19 @@ var builder = WebApplication.CreateBuilder(args);
 builder.Services.AddSingleton<SynonymService>();
 
 builder.Services.AddControllers();
+
+// Configure CORS
+builder.Services.AddCors(options =>
+{
+    options.AddPolicy(name: MyAllowSpecificOrigins,
+                      policy  =>
+                      {
+                          policy.WithOrigins("https://master.d3j75al23adf2c.amplifyapp.com")
+                             .AllowAnyMethod()
+                             .AllowAnyHeader();
+                      });
+});
+
 // Learn more about configuring Swagger/OpenAPI at https://aka.ms/aspnetcore/swashbuckle
 builder.Services.AddEndpointsApiExplorer();
 builder.Services.AddSwaggerGen();
@@ -20,6 +35,8 @@ if (app.Environment.IsDevelopment())
 }
 
 app.UseHttpsRedirection();
+
+app.UseCors(MyAllowSpecificOrigins);
 
 app.UseAuthorization();
 
